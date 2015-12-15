2ce568e949965695be66b0dae3878278816beca3
cwen <cwen@regenstrief.org>
2015-12-15 16:35:50 -0500

    OHDSI-59 =>
    - PanaceaStageCombinationMapper, PanaceaStageCombination,
    PanaceaStageCombinationMap
    - PanaceaStageCombinationMapRepository,
    PanaceaStageCombinationRepository
    - PanaceaService: add logger, combination/combination map methods
    - unit test, PanaceaTest( ignored by default)

--- src/main/java/org/ohdsi/webapi/panacea/repository/impl/PanaceaService.java
+++ src/main/java/org/ohdsi/webapi/panacea/repository/impl/PanaceaService.java
@@ -12,6 +12,8 @@
  */
 package org.ohdsi.webapi.panacea.repository.impl;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -21,9 +23,20 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.ohdsi.sql.SqlRender;
+import org.ohdsi.sql.SqlTranslate;
+import org.ohdsi.webapi.panacea.mapper.PanaceaStageCombinationMapper;
+import org.ohdsi.webapi.panacea.pojo.PanaceaStageCombination;
+import org.ohdsi.webapi.panacea.pojo.PanaceaStageCombinationMap;
 import org.ohdsi.webapi.panacea.pojo.PanaceaStudy;
+import org.ohdsi.webapi.panacea.repository.PanaceaStageCombinationMapRepository;
+import org.ohdsi.webapi.panacea.repository.PanaceaStageCombinationRepository;
 import org.ohdsi.webapi.panacea.repository.PanaceaStudyRepository;
 import org.ohdsi.webapi.service.AbstractDaoService;
+import org.ohdsi.webapi.source.Source;
+import org.ohdsi.webapi.source.SourceDaimon;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -34,8 +47,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class PanaceaService extends AbstractDaoService {
     
+    private static final Log log = LogFactory.getLog(PanaceaService.class);
+    
     @Autowired
     private PanaceaStudyRepository panaceaStudyRepository;
+    
+    @Autowired
+    private PanaceaStageCombinationRepository pncStageCombinationRepository;
+    
+    @Autowired
+    private PanaceaStageCombinationMapRepository pncStageCombinationMapRepository;
     
     @Autowired
     private EntityManager em;
@@ -81,6 +102,101 @@ public class PanaceaService extends AbstractDaoService {
     }
     
     /**
+     * Get PanaceaStageCombination by id
+     * 
+     * @param pncStageCombId Long
+     * @return PanaceaStageCombination
+     */
+    @GET
+    @Path("/pncstudycombination/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public PanaceaStageCombination getPanaceaStageCombinationById(@PathParam("id") final Long pncStageCombId) {
+        
+        final PanaceaStageCombination pncStgCmb = this.getPncStageCombinationRepository().getPanaceaStageCombinationById(
+            pncStageCombId);
+        return pncStgCmb;
+    }
+    
+    /**
+     * Get PanaceaStageCombination by studyId
+     * 
+     * @param studyId Long
+     * @return List of PanaceaStageCombination
+     */
+    @GET
+    @Path("/pncstudycombinationforstudy/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<PanaceaStageCombination> getPanaceaStageCombinationByStudyId(@PathParam("id") final Long studyId) {
+        String sql = "select PNC_TX_STG_CMB_ID, STUDY_ID from @panacea_schema.pnc_tx_stage_combination where STUDY_ID = @studyId ";
+        sql = SqlRender.renderSql(sql, new String[] { "panacea_schema", "studyId" }, new String[] { getOhdsiSchema(),
+                studyId.toString() });
+        sql = SqlTranslate.translateSql(sql, getSourceDialect(), getDialect());
+        return this.getJdbcTemplate().query(sql, new PanaceaStageCombinationMapper());
+    }
+    
+    /**
+     * Get PanaceaStageCombinationMap by id
+     * 
+     * @param pncStageCombMpId Long
+     * @return PanaceaStageCombinationMap
+     */
+    @GET
+    @Path("/pncstudycombinationmap/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public PanaceaStageCombinationMap getPanaceaStageCombinationMapById(@PathParam("id") final Long pncStageCombMpId) {
+        
+        final PanaceaStageCombinationMap pncStgCmbMp = this.getPncStageCombinationMapRepository()
+                .getPanaceaStageCombinationMapById(pncStageCombMpId);
+        return pncStgCmbMp;
+    }
+
+    /**
+     * Get PanaceaStageCombination by studyId
+     * 
+     * @param studyId Long
+     * @return List of PanaceaStageCombination
+     */
+    @GET
+    @Path("/pncstudycombinationforstudy/{sourceKey}/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<PanaceaStageCombination> getPanaceaStageCombinationByStudyId(@PathParam("sourceKey") final String sourceKey,
+                                                                             @PathParam("id") final Long studyId) {
+        /**
+         * with sourceKey as a parameter. I don't feel we need the multi-source part for Panacea.
+         * Will need to discuss with Jon later.
+         */
+        final Source source = getSourceRepository().findBySourceKey(sourceKey);
+        final String tableQualifier = source.getTableQualifier(SourceDaimon.DaimonType.Results);
+        String sql = "select PNC_TX_STG_CMB_ID, STUDY_ID from @panacea_schema.pnc_tx_stage_combination where STUDY_ID = @studyId ";
+        sql = SqlRender.renderSql(sql, new String[] { "panacea_schema", "studyId" },
+            new String[] { tableQualifier, studyId.toString() });
+        sql = SqlTranslate.translateSql(sql, source.getSourceDialect(), source.getSourceDialect());
+        return this.getSourceJdbcTemplate(source).query(sql, new PanaceaStageCombinationMapper());
+    }
+    
+    //    /**
+    //     * Create a new PanaceaStageCombination and save for a study
+    //     * 
+    //     * @param studyId Long
+    //     * @return PanaceaStageCombination
+    //     */
+    //    @POST
+    //    @Path("/newstudystagecombination")
+    //    @Produces(MediaType.APPLICATION_JSON)
+    //    @Consumes(MediaType.APPLICATION_JSON)
+    //    public PanaceaStageCombination createStudyStageCombination(final Long studyId) {
+    //        if (studyId != null) {
+    //            final PanaceaStageCombination pncComb = new PanaceaStageCombination();
+    //            pncComb.setStudyId(studyId);
+    //            
+    //            return this.pncStageCombinationRepository.save(pncComb);
+    //        } else {
+    //            log.error("Study ID is null in PanaceaService.createStudyStageCombination.");
+    //            return null;
+    //        }
+    //    }
+    
+    /**
      * @return the panaceaStudyRepository
      */
     public PanaceaStudyRepository getPanaceaStudyRepository() {
@@ -93,4 +209,47 @@ public class PanaceaService extends AbstractDaoService {
     public void setPanaceaStudyRepository(final PanaceaStudyRepository panaceaStudyRepository) {
         this.panaceaStudyRepository = panaceaStudyRepository;
     }
+    
+    /**
+     * @return the pncStageCombinationRepository
+     */
+    public PanaceaStageCombinationRepository getPncStageCombinationRepository() {
+        return this.pncStageCombinationRepository;
+    }
+    
+    /**
+     * @param pncStageCombinationRepository the pncStageCombinationRepository to set
+     */
+    public void setPncStageCombinationRepository(final PanaceaStageCombinationRepository pncStageCombinationRepository) {
+        this.pncStageCombinationRepository = pncStageCombinationRepository;
+    }
+    
+    /**
+     * @return the pncStageCombinationMapRepository
+     */
+    public PanaceaStageCombinationMapRepository getPncStageCombinationMapRepository() {
+        return this.pncStageCombinationMapRepository;
+    }
+    
+    /**
+     * @param pncStageCombinationMapRepository the pncStageCombinationMapRepository to set
+     */
+    public void setPncStageCombinationMapRepository(final PanaceaStageCombinationMapRepository pncStageCombinationMapRepository) {
+        this.pncStageCombinationMapRepository = pncStageCombinationMapRepository;
+    }
+    
+    /**
+     * @return the em
+     */
+    public EntityManager getEm() {
+        return this.em;
+    }
+    
+    /**
+     * @param em the em to set
+     */
+    public void setEm(final EntityManager em) {
+        this.em = em;
+    }
+    
 }
---
+++ src/test/java/org/ohdsi/webapi/test/PanaceaTest.java
@@ -0,0 +1,98 @@
+/**
+ * The contents of this file are subject to the Regenstrief Public License
+ * Version 1.0 (the "License"); you may not use this file except in compliance with the License.
+ * Please contact Regenstrief Institute if you would like to obtain a copy of the license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) Regenstrief Institute.  All Rights Reserved.
+ */
+package org.ohdsi.webapi.test;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ohdsi.webapi.WebApi;
+import org.ohdsi.webapi.panacea.pojo.PanaceaStageCombination;
+import org.ohdsi.webapi.panacea.pojo.PanaceaStageCombinationMap;
+import org.ohdsi.webapi.panacea.repository.impl.PanaceaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Document curl WS testing:
+ * 
+ * <pre>
+ *  curl -X GET -H "Content-Type: application/json" http://localhost:8080/WebAPI/panacea/2
+ *  
+ *  curl -X POST -H "Content-Type: application/json" -d '{"studyName":"Test adding study","studyDesc":"test as proof of concept","concepSetDef":"{\"items\" :[{\"concept\":{\"CONCEPT_ID\":1301025,\"CONCEPT_NAME\":\"Enoxaparin\",\"STANDARD_CONCEPT\":\"S\",\"INVALID_REASON\":\"V\",\"CONCEPT_CODE\":\"67108\",\"DOMAIN_ID\":\"Drug\",\"VOCABULARY_ID\":\"RxNorm\",\"CONCEPT_CLASS_ID\":\"Ingredient\",\"INVALID_REASON_CAPTION\":\"Valid\",\"STANDARD_CONCEPT_CAPTION\":\"Standard\",\"RECORD_COUNT\":\"17\",\"DESCENDANT_RECORD_COUNT\":\"17\"},\"isExcluded\":false,\"includeDescendants\":false,\"includeMapped\":false},{\"concept\":{\"CONCEPT_ID\":1328165,\"CONCEPT_NAME\":\"Diltiazem\",\"STANDARD_CONCEPT\":\"S\",\"INVALID_REASON\":\"V\",\"CONCEPT_CODE\":\"3443\",\"DOMAIN_ID\":\"Drug\",\"VOCABULARY_ID\":\"RxNorm\",\"CONCEPT_CLASS_ID\":\"Ingredient\",\"INVALID_REASON_CAPTION\":\"Valid\",\"STANDARD_CONCEPT_CAPTION\":\"Standard\",\"RECORD_COUNT\":\"473\",\"DESCENDANT_RECORD_COUNT\":\"984\"},\"isExcluded\":false,\"includeDescendants\":false,\"includeMapped\":false},{\"concept\":{\"CONCEPT_ID\":1771162,\"CONCEPT_NAME\":\"Cefazolin\",\"STANDARD_CONCEPT\":\"S\",\"INVALID_REASON\":\"V\",\"CONCEPT_CODE\":\"2180\",\"DOMAIN_ID\":\"Drug\",\"VOCABULARY_ID\":\"RxNorm\",\"CONCEPT_CLASS_ID\":\"Ingredient\",\"INVALID_REASON_CAPTION\":\"Valid\",\"STANDARD_CONCEPT_CAPTION\":\"Standard\",\"RECORD_COUNT\":\"86\",\"DESCENDANT_RECORD_COUNT\":\"172\"},\"isExcluded\":false,\"includeDescendants\":false,\"includeMapped\":false},{\"concept\":{\"CONCEPT_ID\":19058274,\"CONCEPT_NAME\":\"Purified Protein Derivative of Tuberculin\",\"STANDARD_CONCEPT\":\"S\",\"INVALID_REASON\":\"V\",\"CONCEPT_CODE\":\"8948\",\"DOMAIN_ID\":\"Drug\",\"VOCABULARY_ID\":\"RxNorm\",\"CONCEPT_CLASS_ID\":\"Ingredient\",\"INVALID_REASON_CAPTION\":\"Valid\",\"STANDARD_CONCEPT_CAPTION\":\"Standard\",\"RECORD_COUNT\":\"0\",\"DESCENDANT_RECORD_COUNT\":\"0\"},\"isExcluded\":false,\"includeDescendants\":false,\"includeMapped\":false},{\"concept\":{\"CONCEPT_ID\":918906,\"CONCEPT_NAME\":\"oxybutynin\",\"STANDARD_CONCEPT\":\"S\",\"INVALID_REASON\":\"V\",\"CONCEPT_CODE\":\"32675\",\"DOMAIN_ID\":\"Drug\",\"VOCABULARY_ID\":\"RxNorm\",\"CONCEPT_CLASS_ID\":\"Ingredient\",\"INVALID_REASON_CAPTION\":\"Valid\",\"STANDARD_CONCEPT_CAPTION\":\"Standard\",\"RECORD_COUNT\":\"117\",\"DESCENDANT_RECORD_COUNT\":\"241\"},\"isExcluded\":false,\"includeDescendants\":false,\"includeMapped\":false},{\"concept\":{\"CONCEPT_ID\":923645,\"CONCEPT_NAME\":\"Omeprazole\",\"STANDARD_CONCEPT\":\"S\",\"INVALID_REASON\":\"V\",\"CONCEPT_CODE\":\"7646\",\"DOMAIN_ID\":\"Drug\",\"VOCABULARY_ID\":\"RxNorm\",\"CONCEPT_CLASS_ID\":\"Ingredient\",\"INVALID_REASON_CAPTION\":\"Valid\",\"STANDARD_CONCEPT_CAPTION\":\"Standard\",\"RECORD_COUNT\":\"419\",\"DESCENDANT_RECORD_COUNT\":\"886\"},\"isExcluded\":false,\"includeDescendants\":false,\"includeMapped\":false},{\"concept\":{\"CONCEPT_ID\":933724,\"CONCEPT_NAME\":\"Phenazopyridine\",\"STANDARD_CONCEPT\":\"S\",\"INVALID_REASON\":\"V\",\"CONCEPT_CODE\":\"8120\",\"DOMAIN_ID\":\"Drug\",\"VOCABULARY_ID\":\"RxNorm\",\"CONCEPT_CLASS_ID\":\"Ingredient\",\"INVALID_REASON_CAPTION\":\"Valid\",\"STANDARD_CONCEPT_CAPTION\":\"Standard\",\"RECORD_COUNT\":\"28\",\"DESCENDANT_RECORD_COUNT\":\"56\"},\"isExcluded\":false,\"includeDescendants\":false,\"includeMapped\":false},{\"concept\":{\"CONCEPT_ID\":1310149,\"CONCEPT_NAME\":\"Warfarin\",\"STANDARD_CONCEPT\":\"S\",\"INVALID_REASON\":\"V\",\"CONCEPT_CODE\":\"11289\",\"DOMAIN_ID\":\"Drug\",\"VOCABULARY_ID\":\"RxNorm\",\"CONCEPT_CLASS_ID\":\"Ingredient\",\"INVALID_REASON_CAPTION\":\"Valid\",\"STANDARD_CONCEPT_CAPTION\":\"Standard\",\"RECORD_COUNT\":\"350\",\"DESCENDANT_RECORD_COUNT\":\"757\"},\"isExcluded\":false,\"includeDescendants\":false,\"includeMapped\":false},{\"concept\":{\"CONCEPT_ID\":1125315,\"CONCEPT_NAME\":\"Acetaminophen\",\"STANDARD_CONCEPT\":\"S\",\"INVALID_REASON\":\"V\",\"CONCEPT_CODE\":\"161\",\"DOMAIN_ID\":\"Drug\",\"VOCABULARY_ID\":\"RxNorm\",\"CONCEPT_CLASS_ID\":\"Ingredient\",\"INVALID_REASON_CAPTION\":\"Valid\",\"STANDARD_CONCEPT_CAPTION\":\"Standard\",\"RECORD_COUNT\":\"1,258\",\"DESCENDANT_RECORD_COUNT\":\"2,821\"},\"isExcluded\":false,\"includeDescendants\":false,\"includeMapped\":false}]}","cohortDefId":915,"studyDetail":"testing","switchWindow":30,"studyDuration":1095,"startDate":"2009-01-01","endDate":"2013-01-01"}' http://localhost:8080/WebAPI/panacea/newstudy
+ *  
+ *  curl -X POST -H "Content-Type: application/json" -d http://localhost:8080/WebAPI/panacea/newstudystagecombination
+ * 
+ * </pre>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WebApi.class)
+@Ignore
+public class PanaceaTest extends TestCase {
+    
+    private static final Log log = LogFactory.getLog(PanaceaTest.class);
+    
+    @Autowired
+    private PanaceaService pncService;
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Test
+    public void testGetCombination() {
+        try {
+            /**
+             * Use browser test too:
+             * http://localhost:8080/WebAPI/panacea/pncstudycombinationforstudy/RIV5/2
+             */
+            List<PanaceaStageCombination> pncStgCmbs = this.pncService.getPanaceaStageCombinationByStudyId("RIV5", new Long(
+                    2));
+            log.info("testGetCombination: " + pncStgCmbs.toString());
+            
+            /**
+             * http://localhost:8080/WebAPI/panacea/pncstudycombinationforstudy/2
+             */
+            pncStgCmbs = this.pncService.getPanaceaStageCombinationByStudyId(new Long(2));
+            log.info("testGetCombination without source: " + pncStgCmbs.toString());
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void testLoadCombinationWithMap() {
+        try {
+            /**
+             * Use browser test too: http://localhost:8080/WebAPI/panacea/pncstudycombination/1
+             */
+            final PanaceaStageCombination pncStgCmb = this.pncService.getPanaceaStageCombinationById(new Long(2));
+            log.info("testLoadCombinationWithMap: " + pncStgCmb.toString());
+            
+            /**
+             * http://localhost:8080/WebAPI/panacea/pncstudycombinationmap/2
+             */
+            final PanaceaStageCombinationMap map = this.pncService.getPanaceaStageCombinationMapById(new Long(2));
+            log.info("testLoadCombinationWithMap map: " + map.toString());
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
