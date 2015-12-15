7d1ebf3601b34573d735d1b0b1a40012164e9ed0
cwen <cwen@regenstrief.org>
2015-12-15 16:35:50 -0500

    OHDSI-59 =>
    - PanaceaStageCombinationMapper, PanaceaStageCombination,
    PanaceaStageCombinationMap
    - PanaceaStageCombinationMapRepository,
    PanaceaStageCombinationRepository
    - PanaceaService: add logger, combination/combination map methods
    - unit test, PanaceaTest( ignored by default)

---
+++ src/main/java/org/ohdsi/webapi/panacea/repository/PanaceaStageCombinationRepository.java
@@ -0,0 +1,31 @@
+/**
+ * The contents of this file are subject to the Regenstrief Public License
+ * Version 1.0 (the "License"); you may not use this file except in compliance with the License.
+ * Please contact Regenstrief Institute if you would like to obtain a copy of the license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) Regenstrief Institute.  All Rights Reserved.
+ */
+package org.ohdsi.webapi.panacea.repository;
+
+import org.ohdsi.webapi.panacea.pojo.PanaceaStageCombination;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ */
+public interface PanaceaStageCombinationRepository extends CrudRepository<PanaceaStageCombination, Long> {
+    
+    /**
+     * 
+     * @param cmbId Long
+     * @return PanaceaStageCombination
+     */
+    @Query("from PanaceaStageCombination where pnc_tx_stg_cmb_id = ?")
+    public PanaceaStageCombination getPanaceaStageCombinationById(Long cmbId);
+}
