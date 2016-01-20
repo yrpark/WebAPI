47021f2a9d5d449fdb0b6024d9f7aedf77a4c580
cwen <cwen@regenstrief.org>
2016-01-20 16:10:16 -0500

    OHDSI-59 => time window overlapping calculation.

--- src/main/java/org/ohdsi/webapi/panacea/repository/impl/PatientStageCombinationCount.java
+++ src/main/java/org/ohdsi/webapi/panacea/repository/impl/PatientStageCombinationCount.java
@@ -78,6 +78,13 @@ public class PatientStageCombinationCount implements Serializable {
      * @param startDate the startDate to set
      */
     public void setStartDate(final Date startDate) {
+        if ((this.getEndDate() != null) && (startDate != null)) {
+            //TODO -- remove or adding formal validation
+            if (startDate.after(this.getEndDate())) {
+                System.out.println("error in setting time: setStartDate:" + startDate + "---" + this.getStartDate() + "---"
+                        + this.endDate);
+            }
+        }
         this.startDate = startDate;
     }
     
@@ -92,10 +99,17 @@ public class PatientStageCombinationCount implements Serializable {
      * @param endDate the endDate to set
      */
     public void setEndDate(final Date endDate) {
+        if ((this.startDate != null) && (endDate != null)) {
+            //TODO -- remove or adding formal validation
+            if (this.startDate.after(endDate)) {
+                System.out.println("error in setting time: setEndDate:" + this.getStartDate() + "---" + this.getEndDate()
+                        + "---" + endDate);
+            }
+        }
         this.endDate = endDate;
     }
     
-    /* (non-Jsdoc)
+    /**
      * @see java.lang.Object#toString()
      */
     @Override
