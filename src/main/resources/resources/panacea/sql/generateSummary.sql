4b074d181ba1492769f285b32a8eae1189676d8d
cwen <cwen@regenstrief.org>
2016-01-26 10:15:37 -0500

    OHDSI-59 OHDSI-62 =>
    - change path "->" to ">"
    - add in step to generate summary in PanaceaService
    - fix generateSummary.sql

--- src/main/resources/resources/panacea/sql/generateSummary.sql
+++ src/main/resources/resources/panacea/sql/generateSummary.sql
@@ -1,16 +1,16 @@
 delete from @results_schema.pnc_study_summary_path where study_id = @studyId and source_id = @sourceId;
 
-insert into @results_schema.pnc_study_summary_path(pnc_stdy_smry_id, study_id, source_id, tx_path_parent_key, tx_stg_cmb, tx_stg_cmb_pth, tx_seq, tx_stg_cnt, tx_stg_avg_dr)
-select seq_pnc_stdy_smry.nextval, @studyId, @sourceId, null, aggregatePath.combo_ids, aggregatePath.combo_seq, aggregatePath.tx_seq, aggregatePath.patientCount, aggregatePath.averageDurationDays 
+insert into @results_schema.pnc_study_summary_path (pnc_stdy_smry_id, study_id, source_id, tx_path_parent_key, tx_stg_cmb, tx_stg_cmb_pth, tx_seq, tx_stg_cnt, tx_stg_avg_dr)
+select seq_pnc_stdy_smry.nextval, 18, 2, null, aggregatePath.combo_ids, aggregatePath.combo_seq, aggregatePath.tx_seq, aggregatePath.patientCount, aggregatePath.averageDurationDays 
 from
-  (select combo_ids combo_ids, combo_seq combo_seq, tx_seq tx_seq, count(*) patientCount, avg(combo_duration) averageDurationDays from #_pnc_tmp_cmb_sq_ct ptTxPath
+  (select combo_ids combo_ids, combo_seq combo_seq, tx_seq tx_seq, count(*) patientCount, avg(combo_duration) averageDurationDays from #_PNC_TMP_CMB_SQ_CT ptTxPath
     group by combo_ids, combo_seq, tx_seq) aggregatePath;
-    
+
 merge into @results_schema.pnc_study_summary_path  m
 using
   (
     select pathsum.rowid as the_rowid, parentpath.pnc_stdy_smry_id as parentKey, updateParentPath.parentPath pPath from @results_schema.pnc_study_summary_path pathSum
-    join (select rowid, SUBSTR(tx_stg_cmb_pth , 0 , length(tx_stg_cmb_pth) - length(tx_stg_cmb) - 2 ) as parentPath
+    join (select rowid, SUBSTR(tx_stg_cmb_pth , 0 , length(tx_stg_cmb_pth) - length(tx_stg_cmb) - 1 ) as parentPath
     from @results_schema.pnc_study_summary_path) updateParentPath
     on updateParentPath.rowid = pathSum.rowid
     join pnc_study_summary_path parentPath
@@ -20,4 +20,4 @@ using
   (
      m.rowid = m1.the_rowid
   )
-WHEN MATCHED then update set m.tx_path_parent_key = m1.parentKey;
+  WHEN MATCHED then update set m.tx_path_parent_key = m1.parentKey;
