e912b636bbae00add68a754a83fb81764c966375
cwen <cwen@regenstrief.org>
2016-04-25 10:43:05 -0400

    OHDSI-67 =>
    - mainly for merging JSON same pathway adjacent same unique concepts
    nodes
    - add PanaceaUtil for JSON manipulation
    - unit test

--- src/test/java/org/ohdsi/webapi/test/PanaceaTest.java
+++ src/test/java/org/ohdsi/webapi/test/PanaceaTest.java
@@ -20,6 +20,8 @@ import junit.framework.TestCase;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,6 +33,7 @@ import org.ohdsi.webapi.panacea.pojo.PanaceaStudy;
 import org.ohdsi.webapi.panacea.pojo.PanaceaSummary;
 import org.ohdsi.webapi.panacea.pojo.PanaceaSummaryLight;
 import org.ohdsi.webapi.panacea.repository.impl.PanaceaService;
+import org.ohdsi.webapi.panacea.repository.impl.PanaceaUtil;
 import org.ohdsi.webapi.service.VocabularyService;
 import org.ohdsi.webapi.vocabulary.Concept;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -178,7 +181,7 @@ public class PanaceaTest extends TestCase {
         log.info("testExpressionResolver: " + procedureConceptIdsStr);
     }
     
-    @Test
+    @Ignore
     public void testGetAllStudy() {
         try {
             /**
@@ -214,12 +217,48 @@ public class PanaceaTest extends TestCase {
         }
     }
     
-    @Test
+    @Ignore
     public void testGetStudySummaryLightList() {
         try {
             final List<PanaceaSummaryLight> summaryList = this.pncService.getStudySummaryLight(new Long(31));
             
             log.info("testGetStudySummaryLightList: " + summaryList);
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Ignore
+    public void testGetStudySummaryAndJSON() {
+        try {
+            final PanaceaSummary summary = this.pncService.getStudySummary(new Long(31), new Integer(1));
+            
+            final JSONObject rootNode = new JSONObject(summary.getStudyResultFiltered());
+            
+            if (rootNode.has("children")) {
+                final JSONArray childJsonArray = rootNode.getJSONArray("children");
+                
+                final JSONArray newChildArray = new JSONArray();
+                
+                for (int i = 0; i < childJsonArray.length(); i++) {
+                    //final JSONObject merged = mergeObj((JSONObject) childJsonArray.get(i));
+                    //                    JSONObject merged = mergeNode((JSONObject) childJsonArray.get(i));
+                    //                    merged = mergeSameUniqueDesedentNode(merged);
+                    
+                    //                    JSONObject merged = mergeSameDesedentNode((JSONObject) childJsonArray.get(i));
+                    final JSONObject merged = PanaceaUtil.mergeNode((JSONObject) childJsonArray.get(i));
+                    
+                    newChildArray.put(merged);
+                }
+                
+                rootNode.remove("children");
+                if (newChildArray.length() > 0) {
+                    rootNode.putOpt("children", newChildArray);
+                }
+            }
+            
+            log.info("testGetStudySummaryAndJSON: " + rootNode);
+            
         } catch (final Exception e) {
             e.printStackTrace();
         }
